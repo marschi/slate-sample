bd3fb09ec0112ac97d6723dddca1897abc667e9f
marschi <majoschi@gmail.com>
2017-03-22 17:47:48 +0100

    Set everything up

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Main';
+
+const rootNode = document.getElementById('root');
+
+ReactDOM.render(
+  <Main />,
+  rootNode
+);
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Main extends React.PureComponent {
+
+  render() {
+    return <div>Insert cool editor here</div>
+  }
+
+}
